b473571858d8f75defea2763c8635b5eb4641429
RazCasanova <f4casanovarosasraziel@gmail.com>
2022-05-31 15:10:36 -0500

    31-05-2022 Implementación de localStorage

--- 38.-practicas-DOM/scripts/darkMode.js
+++ 38.-practicas-DOM/scripts/darkMode.js
@@ -3,19 +3,42 @@ export default function modoOscuro(btn_dark_mode, classDark) {
     $selectores = document.querySelectorAll('[data-dark]');
     let sol = "🌞",
         luna = "🌙";
+
+
+    const lightMode = () =>{
+        $selectores.forEach(element => {
+            element.classList.remove(classDark);
+        });
+        $btn_dark_mode.children[0].textContent = luna;
+        localStorage.setItem('theme','light');
+    };
+
+    const darkMode = () =>{
+        $selectores.forEach(element => {
+            element.classList.add(classDark);
+        });
+        $btn_dark_mode.children[0].textContent = sol;
+        localStorage.setItem('theme','dark');
+    };
     document.addEventListener('click', (evt) =>{
         if (evt.target.matches('.btn-dark-mode') || evt.target.matches('.btn-dark-mode *')) {
-            if (!document.documentElement.classList.contains('dark-mode')) {
-                $selectores.forEach(element => {
-                    element.classList.add(classDark);
-                });
-                $btn_dark_mode.children[0].textContent = sol;
+            if ($btn_dark_mode.children[0].textContent === luna) {
+                darkMode();
             }else{
-                $btn_dark_mode.children[0].textContent = luna;
-                $selectores.forEach(element => {
-                    element.classList.remove(classDark);
-                });
+                lightMode();
             }
+        }
+    });
+/* No es posible invocar un evento de un mismo tipo detro de otro, por lo que es necesario mover la invocación de la función fuera del evento */
+    document.addEventListener('DOMContentLoaded', (evt)=>{
+        if (localStorage.getItem('theme') === null) {
+            localStorage.setItem('theme','light');
+        }
+        if (localStorage.getItem('theme') === 'light') {
+            lightMode();
+        }
+        if (localStorage.getItem('theme') === 'dark') {
+            darkMode();
         }
     });
 }
